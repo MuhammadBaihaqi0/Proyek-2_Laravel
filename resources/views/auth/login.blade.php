dee15ce118244335b0bd2f7920583d057ad4b2d9
Muhammad Baihaqi Siregar <baihaqisiregar09@gmail.com>
2025-11-24 21:00:08 +0700

    capctcha

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,6 +40,16 @@
                 </label>
             </div>
 
+            <div class="mt-4">
+                <x-input-label for="captcha" value="Kode Keamanan" />
+                <div class="flex items-center mt-1">
+                    <span class="mr-3">{!! captcha_img('flat') !!}</span>
+                    <x-text-input id="captcha" class="block w-full" type="text" name="captcha" required
+                        placeholder="Masukkan kode di samping" />
+                </div>
+                <x-input-error :messages="$errors->get('captcha')" class="mt-2" />
+            </div>
+
             <div class="flex items-center justify-end mt-4">
 
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mr-4"
